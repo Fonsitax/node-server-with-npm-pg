389af259026b742d378aa65766f6d24793950cb7
Fonsitax <mynameisfonja@gmail>
2024-07-19 22:10:53 +0200

    some work around

--- crudOperations.js
+++ crudOperations.js
@@ -1,12 +1,20 @@
 // Import utility functions
 import { getResourceId, processBodyFromRequest, returnErrorWithMessage } from './utils.js';
 import pg from 'pg';
+const { Client } = pg;
 
+
+// Create a new post
 export const createPost = async (req, res) => {
   try {
     const body = await processBodyFromRequest(req);
     if (!body) return returnErrorWithMessage(res, 400, 'Body is required');
     const parsedBody = JSON.parse(body);
+
+    // Connect to the database
+    const client = new Client({
+      connectionString: process.env.PG.URI,
+    });
     console.log('Here we have access to the body: ', parsedBody);
     res.statusCode = 201;
     res.setHeader('Content-Type', 'application/json');
